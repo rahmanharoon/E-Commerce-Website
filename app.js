7711b89b2a2bcbdbbd77e39537e5c64cd5a9b98f
Rahman Haroon <rahmanharoon128@gmail.com>
2020-09-24 22:25:40 +0530

    add product page updated

--- app.js
+++ app.js
@@ -7,8 +7,9 @@ var logger = require('morgan');
 var userRouter = require('./routes/user');
 var adminRouter = require('./routes/admin');
 var hbs=require('express-handlebars')
-
+var fileUpload=require('express-fileupload')
 var app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,10 +21,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(fileUpload())
 
 app.use('/', userRouter);
 app.use('/admin', adminRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
