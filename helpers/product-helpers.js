935a1dd311fb4654cee7456ff118e582bfbf89a3
Rahman Haroon <rahmanharoon128@gmail.com>
2020-09-25 21:35:38 +0530

    un usual console removed

--- helpers/product-helpers.js
+++ helpers/product-helpers.js
@@ -3,7 +3,7 @@ var db = require('../config/connection')
 module.exports={
 
     addProduct:(product,callback)=> {
-        console.log(product);
+        
 
         db.get().collection('product').insertOne(product).then((data)=> {
             
