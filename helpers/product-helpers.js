acaa0a104a6ed22c9ad4b55f49fc534b3b9023cd
Rahman Haroon <rahmanharoon128@gmail.com>
2020-09-26 19:40:57 +0530

    promoise

--- helpers/product-helpers.js
+++ helpers/product-helpers.js
@@ -1,5 +1,5 @@
 var db = require('../config/connection')
-
+var collection=require('../config/collections')
 module.exports={
 
     addProduct:(product,callback)=> {
@@ -10,7 +10,11 @@ module.exports={
             callback(data.ops[0]._id)
 
         })
+    },
+
+    getAllProducts:()=>{
+        return new Promise((resolve,reject)=>{
+            let products=db.get().collection(collection.PRODUCT_COLLECTION).find()
+        })
     }
-
-
 }
