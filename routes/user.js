94d1666d65a28370ea17a64d6fc8c92361b0b4b3
Rahman Haroon <rahmanharoon128@gmail.com>
2020-09-27 19:57:10 +0530

    user view page updated

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-const productHelpers = require('../helpers/product-helpers');
+const productHelper = require('../helpers/product-helpers');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   
