ffe66861202dfb3be5e49e299fb5724c11036ccd
Rahman Haroon <rahmanharoon128@gmail.com>
2020-09-27 19:52:25 +0530

    user path committed

--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var router = express.Router();
-
+const productHelpers = require('../helpers/product-helpers');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   
   productHelper.getAllProducts().then((products)=>{
     console.log(products);
-    res.render('admin/view-products',{admin:true,products})
+    res.render('',{admin:true,products})
   })
 });
 
