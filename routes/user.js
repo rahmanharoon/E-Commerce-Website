e63c03c8b16544cf9b4c80b57e746b391182f98e
Rahman Haroon <rahmanharoon128@gmail.com>
2020-10-06 20:54:29 +0530

    user signup succes

--- routes/user.js
+++ routes/user.js
@@ -3,17 +3,17 @@ var router = express.Router();
 const productHelpers = require('../helpers/product-helpers');
 const userHelpers = require('../helpers/user-helpers');
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  
-  productHelpers.getAllProducts().then((products)=>{
-    res.render('user/view-products',{ products })
+router.get('/', function (req, res, next) {
+
+  productHelpers.getAllProducts().then((products) => {
+    res.render('user/view-products', { products })
   })
 });
 
-router.get('/login',(req,res)=> {
+router.get('/login', (req, res) => {
   res.render('user/login')
 })
-router.get('/signup', (req,res)=> {
+router.get('/signup', (req, res) => {
   res.render('user/signup')
 })
 router.post('/signup', (req,res)=> {
@@ -21,8 +21,8 @@ router.post('/signup', (req,res)=> {
     console.log(response);
   })
 })
-router.post('/login',(req,res)=> {
-  userHelpers.doLogin(req.body)
-})
+// router.post('/login',(req,res)=> {
+//   userHelpers.doLogin(req.body)
+// })
 
 module.exports = router;
