7526e35ef7289962bbf33a2dd48ab2f5a9e69e78
Rahman Haroon <rahmanharoon128@gmail.com>
2020-10-08 22:35:05 +0530

    delete button worked fine

--- helpers/product-helpers.js
+++ helpers/product-helpers.js
@@ -1,5 +1,9 @@
 var db = require('../config/connection')
 var collection=require('../config/collections')
+const { ObjectID } = require('bson')
+var ObjectId = require('mongodb').ObjectID
+// const { resolve } = require('path')
+// const { Db } = require('mongodb')
 module.exports={
 
     addProduct:(product,callback)=> {
@@ -16,6 +20,13 @@ module.exports={
         return new Promise(async(resolve,reject)=>{
             let products=await db.get().collection(collection.PRODUCT_COLLECTION).find().toArray()
             resolve(products)
+        })
+    },
+    deleteProduct:(proId)=> {
+        return new Promise((resolve,reject)=> {
+            db.get().collection(collection.PRODUCT_COLLECTION).removeOne({_id:ObjectID(proId)}).then((response)=> {
+                resolve(response)
+            })
         })
     }
 }
--- routes/admin.js
+++ routes/admin.js
@@ -1,3 +1,4 @@
+const { response } = require('express');
 var express = require('express');
 const productHelpers = require('../helpers/product-helpers');
 var router = express.Router();
@@ -33,6 +34,9 @@ router.post('/add-product',(req,res)=>{
 router.get('/delete-product/:id',(req,res)=> {
   let proId = req.params.id
   console.log(proId);
+  productHelpers.deleteProduct(proId).then((response)=> {
+    res.redirect('/admin/')
+  })
 })
 
 module.exports = router;
