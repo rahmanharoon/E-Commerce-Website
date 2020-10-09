38211f9aa9ff32213ceb0ad45c51787d7af485fa
Rahman Haroon <rahmanharoon128@gmail.com>
2020-10-09 19:56:19 +0530

    product editing feild succes

--- helpers/product-helpers.js
+++ helpers/product-helpers.js
@@ -1,6 +1,7 @@
 var db = require('../config/connection')
 var collection=require('../config/collections')
 const { ObjectID } = require('bson')
+const { resolve } = require('path')
 var ObjectId = require('mongodb').ObjectID
 // const { resolve } = require('path')
 // const { Db } = require('mongodb')
@@ -33,6 +34,21 @@ module.exports={
         return new Promise((resolve,reject)=> {
             db.get().collection(collection.PRODUCT_COLLECTION).findOne({_id:ObjectID(proId)}).then((product)=> {
                 resolve(product)
+            })
+        })
+    },
+    updateProduct:(proId,proDetails)=> {
+        return new Promise((resolve,reject)=> {
+            db.get().collection(collection.PRODUCT_COLLECTION)
+            .updateOne({_id:ObjectID(proId)},{
+                $set:{
+                    Name:proDetails.Name,
+                    Description:proDetails.Description,
+                    Price:proDetails.Price,
+                    Category:proDetails.Category
+                }
+            }).then((response)=>{
+                resolve()
             })
         })
     }
