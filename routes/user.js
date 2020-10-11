bd46fc7846fb8788ab05218ce8fb93a210658e39
Rahman Haroon <rahmanharoon128@gmail.com>
2020-10-11 21:44:31 +0530

    items shown in cart

--- routes/user.js
+++ routes/user.js
@@ -59,7 +59,7 @@ router.get('/logout',(req,res)=> {
 router.get('/cart',verifyLogin,async(req,res)=> {
   let products=await userHelpers.getCartProducts(req.session.user._id)
   console.log(products);
-  res.render('user/cart')
+  res.render('user/cart',{products})
 })
 
 router.get('/add-to-cart/:id',verifyLogin,(req,res)=>{
