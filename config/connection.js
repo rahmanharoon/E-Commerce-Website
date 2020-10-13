912a69c526282d12fae19c2ee245e483000144ef
Rahman Haroon <rahmanharoon128@gmail.com>
2020-10-13 20:22:03 +0530

    cart page align committed

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,6 @@ const state={
 module.exports.connect=function(done){
     const url='mongodb://localhost:27017'
     const dbname='shopping'
-
     mongoClient.connect(url,(err,data)=>{
         if (err) return done(err)
         state.db = data.db(dbname)
