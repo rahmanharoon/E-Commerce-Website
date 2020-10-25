fdaf6d73b2a15d475d8613950d325ec884d3c26b
Rahman Haroon <rahmanharoon128@gmail.com>
2020-10-25 21:28:33 +0530

    query for taking total committed

--- routes/user.js
+++ routes/user.js
@@ -79,7 +79,8 @@ router.post('/change-product-quantity', (req,res,next)=>{
     res.json(response)
   })
 })
-router.get('/place-order',(req,res)=>{
+router.get('/place-order',verifyLogin, async(req,res)=>{
+  let total = await userHelpers.getTotalAmount(req.session.user._id)
   res.render('user/place-order')
 })
 
