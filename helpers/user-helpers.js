3fee7b67db43a8357e8864e86a69a1f1f41fd646
Rahman Haroon <rahmanharoon128@gmail.com>
2020-10-25 20:01:37 +0530

    ajax comitted

--- helpers/user-helpers.js
+++ helpers/user-helpers.js
@@ -125,17 +125,16 @@ module.exports = {
     changeProductQuantity:(details)=>{
         details.count=parseInt(details.count)
         details.quantity=parseInt(details.quantity)
-        
+
         return new Promise((resolve,reject)=>{
             if(details.count==-1 && details.quantity==1){
             db.get().collection(collection.CART_COLLECTION)
             .updateOne({ _id:objectId(details.cart)},
                 {
-                    $pull:{products:{item:objectId(details.product) }},
-                    $inc: { 'products.$.quantity': details.count }
+                    $pull:{products:{item:objectId(details.product)}},
+                    // $inc: { 'products.$.quantity': details.count }
                 }
             ).then((response)=>{
-                console.log(response);
                 resolve({removeProduct:true})
             })
         }else{
@@ -143,7 +142,8 @@ module.exports = {
             .updateOne({_id:objectId(details.cart), 'products.item':objectId(details.product)},
             {
                 $inc:{'products.$.quantity':details.count}
-            }).then((response)=>{
+            }
+            ).then((response)=>{
                 resolve(true)
             })
         }
