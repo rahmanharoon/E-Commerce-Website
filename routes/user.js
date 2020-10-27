0ee36f7f87099a1ac0d2380c9d2cc1d5807cdeda
Rahman Haroon <rahmanharoon128@gmail.com>
2020-10-27 21:26:21 +0530

    router for user order comitted

--- routes/user.js
+++ routes/user.js
@@ -97,4 +97,8 @@ router.post('/place-order',async(req,res)=>{
 router.get('/order-success',(req,res)=>{
   res.render('user/order-success',{user:req.session.user})
 })
+router.get('/orders',async (req,res)=>{
+  let orders =  await userHelpers.getUserOrders(req.session.user._id)
+  res.render('user/orders',{user:req.session.user,orders})
+})
 module.exports = router;
